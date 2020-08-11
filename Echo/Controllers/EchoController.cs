0caa2000dee8398e8155b9281044597d16f5184c
gicherui <gideon.cheruiyot@microsoft.com>
2020-08-11 03:02:58 -0700

    first commit

--- Echo/Controllers/EchoController.cs
+++ Echo/Controllers/EchoController.cs
@@ -1,6 +1,15 @@
+
 using System.Collections.Generic;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using static System.Net.Http.HttpResponseHeadersExtensions;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Echo.Controllers
 {
@@ -14,6 +23,7 @@ namespace Echo.Controllers
         {
             _logger = logger;
         }
+
 
         [HttpGet]
         public IDictionary<string, string> Get()
@@ -31,6 +41,18 @@ namespace Echo.Controllers
             return res;
         }
 
+        [HttpGet("~/index")]
+        public HttpResponseHeaders Get1()
+        {
+            var resp = new HttpResponseMessage();
+
+            var cookie = new CookieHeaderValue("session-id", "12345");
+            resp.Headers.AddCookies(new List<CookieHeaderValue>() { cookie });
+
+            return resp.Headers;
+        }
+
+
         [HttpPost]
         public IDictionary<string, string> Post(IDictionary<string, string> body)
         {
@@ -42,6 +64,20 @@ namespace Echo.Controllers
             }
 
             return res;
+        }
+
+        [HttpPost("~/index")]
+        public HttpResponseMessage Post2()
+        {
+            var resp = new HttpResponseMessage();
+
+            var cookie = new CookieHeaderValue("session-id", "12345");
+            IEnumerable<CookieHeaderValue> lst = new List<CookieHeaderValue>();
+            lst.Append(cookie);
+
+            resp.Headers.AddCookies(lst);
+            return resp;
+
         }
     }
 }
--- Echo/Startup.cs
+++ Echo/Startup.cs
@@ -10,11 +10,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
 
 namespace Echo
 {
     public class Startup
     {
+        public const string CookieScheme = "TestingScheme";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +31,28 @@ namespace Echo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+                    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+                    {
+                        options.LoginPath = "/auth";
+                    //https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.cookiebuilder?view=aspnetcore-2.1
+                    options.Cookie = new CookieBuilder
+                        {
+                            Name = "CustomCookie",
+                            HttpOnly = false
+                        };
+                    });
+
+            // Example of how to customize a particular instance of cookie options and
+            // is able to also use other services.
+            services.AddSingleton<IConfigureOptions<CookieAuthenticationOptions>, ConfigureMyCookie>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,15 +67,36 @@ namespace Echo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.MapWhen(IsPF, app =>
             {
-                endpoints.MapControllers();
-                endpoints.MapFallbackToController("Get", "Echo");
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToController("Get", "Echo");
+                });
             });
+
+        }
+
+        private bool IsPF(HttpContext context)
+        {
+            KeyValuePair<string, string> cookie = new KeyValuePair<string, string>("Cookie", "Cooki2");
+   
+            var cookies = context.Request.Cookies;
+            cookies.Append(cookie);
+
+            var headers = context.Request.Headers;
+            if (!headers.ContainsKey("Cookie"))
+            {
+                headers.Add("Cookie", "Cookie header1");
+                headers.Add("Header2", "PFS header2");
+            }
+            
+            return true;
         }
     }
 }
